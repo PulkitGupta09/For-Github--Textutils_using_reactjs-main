9b563ed7df7f41288b15c5f49f98356e389beea5
PulkitGupta09 <89147343+PulkitGupta09@users.noreply.github.com>
2022-04-07 17:24:02 +0530

    For deploying

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import './App.css';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import Alert from './components/Alert';
+import TextForm from './components/TextForm';
+import React,{useState} from 'react';
+
+// import { render } from "react-dom";
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
+function App() {
+  const [mode,setMode] = useState('light')
+  const [alert, setAlert] = useState(null)
+
+  const showAlert =(message,type)=>{
+      setAlert ({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+        setAlert(null);
+      }, 1500);
+  }
+
+  const toggleMode =()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#3B3B3B';
+      showAlert("Dark Mode has been enabled", "success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode has been enabled", "success");
+    }
+  }
+  return (
+    <>
+    {/* <BrowserRouter> */}
+<Navbar title = "TextUtils" about = "About Us" mode = {mode} toggleMode = {toggleMode}/>
+<Alert alert = {alert}/>
+<div className="container my-3" >
+{/* <Routes> */}
+          {/* <Route exact path="/About" */}
+            {/* element = {<About />}/> */}
+          {/* <About /> */}
+          {/* <Route exact path="/" */}
+          <TextForm heading = "Enter the text to analyze below" mode = {mode} showAlert = {showAlert}/>
+          {/* /> */}
+          {/* </Route> */}
+{/* </Routes> */}
+</div>
+{/* </BrowserRouter> */}
+    </>
+    );
+}
+
+export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,88 @@
+import React,{useState} from 'react';
+// import { CopyToClipboard } from "react-copy-to-clipboard";
+
+export default function TextForm(props) {
+    const handleUpClick = ()=>{
+        // console.log("UpperCase was clicked");
+        let newText = text.toUpperCase();
+        setText(newText); 
+        props.showAlert("Converted to Uppercase!","success");
+    }
+    const handleLoClick = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText); 
+        props.showAlert("Converted to Lowercase!","success");
+    }
+    const handleClearText = ()=>{
+        let newText = ""
+        setText(newText); 
+        props.showAlert("Converted to Cleartext!","success");
+    }
+
+    const RemovePunctuation = ()=>{
+            let puncs = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+            let result = text.replace(puncs, '');
+            let newText = result;
+            setText(newText);
+            props.showAlert("Converted to RemovedPunctuation!","success");
+    }
+    const RemoveNumber = ()=>{
+            let num = /[0123456789]/g;
+            let result = text.replace(num, '');
+            let newText = result;
+            setText(newText);
+            props.showAlert("Converted to RemovedNumber!","success");
+    }
+
+    // Copy to Clipboard
+    // const handleCopyClick = ({ alert }) => {
+    //     setIsCopied(true);
+    //     setTimeout(() => {
+    //         setIsCopied(false);
+    //     }, 1000);
+    // }
+
+    const handleOnChange = (event)=>{
+        console.log("On Change");
+        setText(event.target.value);
+    }
+
+
+
+    const [text, setText] = useState("");
+    // const [isCopied, setIsCopied] = useState(false);
+
+    return (
+        <>
+        <div className='container' style = {{color : props.mode==='dark'?'white':'black'}}>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+                <textarea className="form-control" style = {{backgroundColor : props.mode==='dark'?'#3B3B3B':'white',color : props.mode==='dark'?'white':'black'}}  value = {text} onChange = {handleOnChange} id="myBox" rows="10"></textarea>
+            </div>
+            <button className = "btn btn-primary" onClick = {handleUpClick}>Convert to Uppercase</button>
+            <button className = "btn btn-primary mx-2" onClick = {handleLoClick}>Convert to Lowercase</button>
+            <button className = "btn btn-primary mx-2" onClick = {RemovePunctuation}>Remove Punctuation</button>
+            <button className = "btn btn-primary mx-2" onClick = {RemoveNumber}>Remove Numbers</button>
+            <button className = "btn btn-primary mx-2" onClick = {handleClearText}>Clear Text</button>
+            {/* <CopyToClipboard text={text}>
+                        <span>
+                            <button className="btn btn-success mx-2" onClick={handleCopyClick}>Copy to Clipboard</button>
+                            <span>{isCopied ? <b> Copied!</b> : false}
+                        </span>
+                        </span>
+            </CopyToClipboard> */}
+        </div>
+        <div className="container" style = {{color : props.mode==='dark'?'white':'black'}}>
+        <div className="container my-3">
+            <h2>Your text summary</h2>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+        </div>
+        <div className="container my-3">
+            <h2>Preview</h2>
+            <p>{text.length>0?text:"Enter Something in the textbox to preview it here"}</p>
+        </div>
+        </div>
+
+        </>
+    )
+}
